3b7971490fdbdefa3b11b8ec74948ac4f52ab6ab
Tumhara Naam <tumhara-email@example.com>
2026-06-03 13:04:36 +0530

    Replace CallX2 with latest feature version

--- CallX2/app/src/main/java/com/callx/app/activities/MainActivity.java
+++ CallX2/app/src/main/java/com/callx/app/activities/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.ViewGroup;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
